65589df86512e07feb2636eb3232d14d2d883c3c
randuck-dev <mail@raphaelneumann.dk>
2024-01-30 21:57:23 +0100

    Git init with basic pipeline

---
+++ src/main.rs
@@ -0,0 +1,34 @@
+use clap::{builder::Str, Parser};
+use serde::{Deserialize, Serialize};
+
+#[derive(Parser, Debug)]
+struct Args {
+    #[arg(short, long)]
+    file: String,
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let args = Args::parse();
+    let file = std::fs::File::open(args.file)?;
+
+    let pipeline: Pipeline = serde_yaml::from_reader(file)?;
+
+    let mut cmd = std::process::Command::new("bash");
+    cmd.arg("-c").arg(&pipeline.jobs[0].run);
+
+    let output = cmd.output()?;
+    println!("Output:\n----\n{}----", String::from_utf8(output.stdout)?);
+
+    Ok(())
+}
+
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
+struct Pipeline {
+    jobs: Vec<Jobs>,
+}
+
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
+struct Jobs {
+    name: String,
+    run: String,
+}
